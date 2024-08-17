255e5055702ca882bde28f702fcc36054ee96c58
98zi <anghunk@gmail.com>
2024-08-17 08:19:57 +0800

    fix: 修复深色模式问题

--- src/app.vue
+++ src/app.vue
@@ -360,13 +360,22 @@ export default {
         $("#menu_suspendedball").show();
       });
 
+      const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      if (isDarkMode) {
+        $("body").addClass("dark-theme");
+      } else {
+        $("body").removeClass("dark-theme");
+      }
+
       const discourse_color_scheme_override = localStorage.getItem(
         "discourse_color_scheme_override"
       );
       if (discourse_color_scheme_override) {
-        $("body").addClass("dark-theme");
-      } else {
-        $("body").removeClass("dark-theme");
+        if (discourse_color_scheme_override == "dark") {
+          $("body").addClass("dark-theme");
+        } else {
+          $("body").removeClass("dark-theme");
+        }
       }
 
       $(".signature-img").each(function () {

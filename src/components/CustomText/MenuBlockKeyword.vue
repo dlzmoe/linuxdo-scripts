699ec537c03ff61b89436122a9a82183c67266ce
dlzmoe <65840178+dlzmoe@users.noreply.github.com>
2024-12-12 15:22:12 +0800

    fix: 优化关键词屏蔽功能，新增屏蔽标签

--- src/components/CustomText/MenuBlockKeyword.vue
+++ src/components/CustomText/MenuBlockKeyword.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="item">
     <div class="tit">
-      {{ sort }}. 关键词屏蔽功能（使用英文，分隔）屏蔽包含关键字的话题和回复
+      {{ sort }}. 关键词屏蔽功能（使用英文，分隔）屏蔽包含关键字的话题标题、标签、回复
     </div>
   </div>
   <textarea v-model="textarea" @input="handleChange"> </textarea>
@@ -37,28 +37,40 @@ export default {
       if (!this.textarea) return;
 
       // 将用户输入的关键字分割为数组，并去除空格和空值
-      const keywords = this.textarea.split(",").map((keyword) => keyword.trim()).filter(Boolean);
+      const keywords = this.textarea
+        .split(",")
+        .map((keyword) => keyword.trim())
+        .filter(Boolean);
       if (keywords.length === 0) return;
 
       // 安全检查 jQuery 的使用，防止元素不可用时出错
       try {
         // 检查话题标题
         $(".topic-list .main-link .raw-topic-link>*")
-            .filter((index, element) => {
-              const text = $(element).text();
-              return keywords.some((keyword) => text.includes(keyword));
-            })
-            .parents("tr.topic-list-item")
-            .hide();
+          .filter((index, element) => {
+            const text = $(element).text();
+            return keywords.some((keyword) => text.includes(keyword));
+          })
+          .parents("tr.topic-list-item")
+          .hide();
+
+        // 检查话题标签
+        $(".topic-list-item .discourse-tags a")
+          .filter((index, element) => {
+            const text = $(element).text();
+            return keywords.some((keyword) => text.includes(keyword));
+          })
+          .parents("tr.topic-list-item")
+          .hide();
 
         // 检查评论回复
         $(".topic-body .cooked")
-            .filter((index, element) => {
-              const text = $(element).text();
-              return keywords.some((keyword) => text.includes(keyword));
-            })
-            .parents(".topic-post")
-            .hide();
+          .filter((index, element) => {
+            const text = $(element).text();
+            return keywords.some((keyword) => text.includes(keyword));
+          })
+          .parents(".topic-post")
+          .hide();
       } catch (error) {
         console.error("init 方法出错：", error);
       }

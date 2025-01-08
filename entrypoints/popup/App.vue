0cc085e1874dd146cdbb0abdbac9c6d87b87b0c2
dlzmoe <65840178+dlzmoe@users.noreply.github.com>
2025-01-08 14:46:09 +0800

    fix: 兼容火狐浏览器

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,6 +1,12 @@
 <template>
   <h2>欢迎使用 Linxudo Scripts 扩展！</h2>
-  <h3>鼠标移动到浏览器最左侧后点击设置按钮，<a href="https://linuxdo-scripts-docs.zishu.me/" target="_blank">使用教程！</a></h3>
+  <h3>
+    鼠标移动到浏览器最左侧后点击设置按钮，<a
+      href="https://linuxdo-scripts-docs.zishu.me/"
+      target="_blank"
+      >使用教程！</a
+    >
+  </h3>
   <div class="flex">
     <button @click="openSetting">收藏夹</button>
     <button @click="goGithub">Github</button>
@@ -14,12 +20,10 @@ export default {
   },
   methods: {
     openSetting() {
-      const extensionID = chrome.runtime.id
-      console.log(extensionID)
-      window.open(
-        'chrome-extension://' + extensionID + '/bookmark.html',
-        '_blank'
-      )
+      const browserAPI = typeof browser !== 'undefined' ? browser : chrome
+      browserAPI.tabs.create({
+        url: browserAPI.runtime.getURL('bookmark.html'),
+      })
     },
     goGithub() {
       window.open('https://github.com/dlzmoe/linuxdo-scripts', '_blank')

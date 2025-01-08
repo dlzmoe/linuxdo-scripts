40ba0f99b77a63b7a11cf5acb56e39f74e2809a6
dlzmoe <65840178+dlzmoe@users.noreply.github.com>
2025-01-08 11:27:34 +0800

    fix: 优化收藏夹功能

--- entrypoints/components/BasicSettings/MenuBookmark.vue
+++ entrypoints/components/BasicSettings/MenuBookmark.vue
@@ -10,64 +10,71 @@
 </template>
 
 <script>
-import $ from "jquery";
+import $ from 'jquery'
 export default {
-  props: ["modelValue", "sort"],
-  emits: ["update:modelValue"],
+  props: ['modelValue', 'sort'],
+  emits: ['update:modelValue'],
   methods: {
     // 提示组件
     messageToast(message) {
-      const messageElement = document.createElement("div");
-      messageElement.className = "messageToast-text";
-      messageElement.innerText = message;
-      document.getElementById("messageToast").appendChild(messageElement);
+      const messageElement = document.createElement('div')
+      messageElement.className = 'messageToast-text'
+      messageElement.innerText = message
+      document.getElementById('messageToast').appendChild(messageElement)
       setTimeout(() => {
-        messageElement.remove();
-      }, 3000);
+        messageElement.remove()
+      }, 3000)
     },
   },
   created() {
     if (this.modelValue) {
-      const vm = this;
+      const vm = this
       setInterval(() => {
-        if ($(".linxudoscripts-bookmark").length < 1) {
-          $(".topic-map__contents").after(
-            '<button class="btn btn-icon-text linxudoscripts-bookmark" type="button"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-bookmark-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17l-6 4v-14a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v5" /><path d="M16 19h6" /><path d="M19 16v6" /></svg><span class="d-button-label">收藏</span></button>'
-          );
+        if ($('.linxudoscripts-bookmark').length < 1) {
+          $('.topic-map__contents').after(
+            '<button class="btn btn-icon-text linxudoscripts-bookmark" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"  fill="none"  stroke="currentColor" stroke-width="2" stroke-linecap="round"  stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-bookmark-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17l-6 4v-14a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v5" /><path d="M16 19h6" /><path d="M19 16v6" /></svg><span class="d-button-label">收藏</span></button>'
+          )
 
-          $(".linxudoscripts-bookmark").click(function () {
-            var title = $(".header-title .topic-link").text().trim();
-            var cate = $(
-              ".categories-wrapper .badge-category__wrapper .badge-category__name"
+          $('.linxudoscripts-bookmark').click(function () {
+            if ($('.header-title .topic-link').text().trim() == '') {
+              vm.messageToast('请将页面往下滑动一点点，再点击收藏！')
+              return false;
+            }
+            var bookmarkDatatitle = $('.header-title .topic-link').text().trim()
+            var bookmarkDatacate = $(
+              '.categories-wrapper .badge-category__wrapper:nth-child(1) .badge-category__name'
             )
               .text()
-              .trim();
-            var tags = $("#topic-title .discourse-tags .discourse-tag.box")
+              .trim()
+            var bookmarkDatatags = $(
+              '#topic-title .discourse-tags .discourse-tag.box'
+            )
               .map(function () {
-                return $(this).text().trim();
+                return $(this).text().trim()
               })
-              .get();
-            var url = "https://linux.do" + $(".header-title .topic-link").attr("href");
+              .get()
+            var bookmarkDataurl =
+              'https://linux.do' + $('.header-title .topic-link').attr('href')
 
             var data = {
-              url: url,
-              title: title,
-              cate: cate,
-              tags: tags,
-            };
+              url: bookmarkDataurl,
+              title: bookmarkDatatitle,
+              cate: bookmarkDatacate,
+              tags: bookmarkDatatags,
+            }
 
-            console.log(data);
+            console.log(data)
 
             chrome.storage.local.set({ bookmarkData: data }, () => {
-              vm.messageToast("收藏成功，请前往收藏夹查看。");
-            });
+              vm.messageToast('收藏成功，请前往收藏夹查看。')
+            })
 
             // 发送消息到后台脚本
-            chrome.runtime.sendMessage({ action: "open_bookmark_page" });
-          });
+            chrome.runtime.sendMessage({ action: 'open_bookmark_page' })
+          })
         }
-      }, 1000);
+      }, 1000)
     }
   },
-};
+}
 </script>

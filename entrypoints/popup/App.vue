2758525f49516aafe38c4dc95e41ee2f877a3636
dlzmoe <65840178+dlzmoe@users.noreply.github.com>
2025-01-14 10:48:34 +0800

    fix: 优化 pupup 菜单逻辑

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -4,12 +4,13 @@
     <li :class="{ act: activeTab === 'news' }" @click="changeTab('news')">最新</li>
     <li :class="{ act: activeTab === 'menu' }" @click="changeTab('menu')">菜单</li>
   </ul>
+  <div class="refresh" @click="refresh"><svg xmlns="http://www.w3.org/2000/svg" width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg></div>
   <div class="container">
     <div class="content" :class="{ act: activeTab === 'hot' }">
-      <HotPosts />
+      <HotPosts :list="hotlist" />
     </div>
     <div class="content" :class="{ act: activeTab === 'news' }">
-      <NewsPosts />
+      <NewsPosts :list="newslist" />
     </div>
     <div class="content" :class="{ act: activeTab === 'menu' }">
       <SettingMenu />
@@ -25,7 +26,9 @@ import SettingMenu from "./components/SettingMenu.vue";
 export default {
   data() {
     return {
-      activeTab: "hot", // 默认显示最热内容
+      activeTab: "news",
+      hotlist: [], // 热门列表
+      newslist: [], // 最新列表
     };
   },
   components: {
@@ -37,6 +40,65 @@ export default {
     changeTab(tab) {
       this.activeTab = tab;
     },
+    // 获取热门列表
+    async getHotPosts() {
+      this.hotlist = [];
+      fetch("https://linux.do/top.json")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.hotlist = data.topic_list.topics.slice(0, 20);
+          localStorage.setItem('hotlist', JSON.stringify(this.hotlist));
+        })
+        .catch((error) => { });
+    },
+    // 获取最新列表
+    async getNewsPosts() {
+      this.newslist = [];
+      fetch("https://linux.do/new.json")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.newslist = data.topic_list.topics.slice(0, 20);
+          localStorage.setItem('newslist', JSON.stringify(this.newslist));
+        })
+        .catch((error) => {});
+    },
+
+    // 刷新
+    refresh() {
+      this.getHotPosts();
+      this.getNewsPosts();
+      localStorage.setItem('Timestamp', Date.now());
+    }
   },
+  created() {
+    const Timestamp = localStorage.getItem('Timestamp', Date.now());
+    if (Timestamp) {
+      const timeDiff = new Date() - Timestamp;
+      if (timeDiff > 300000) { // 五分钟
+        localStorage.removeItem('hotlist');
+        localStorage.removeItem('newslist');
+        localStorage.removeItem('Timestamp');
+      }
+    } else {
+      localStorage.setItem('Timestamp', Date.now());
+    }
+
+    const hotlist = localStorage.getItem('hotlist');
+    if (hotlist) {
+      this.hotlist = JSON.parse(hotlist);
+    } else {
+      this.getHotPosts(); // 获取热门列表
+    }
+
+    const newslist = localStorage.getItem('newslist');
+    if (newslist) {
+      this.newslist = JSON.parse(newslist);
+    } else {
+      this.getNewsPosts(); // 获取最新列表
+    }
+
+  }
 };
 </script>
--- entrypoints/popup/components/NewsPosts.vue
+++ entrypoints/popup/components/NewsPosts.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="refresh" @click="getHotPosts"><svg xmlns="http://www.w3.org/2000/svg" width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg></div>
-  <ul class="list" v-if="this.list.length>0">
+  <ul class="list" v-if="this.list.length > 0">
     <li v-for="item in list" :key="item.id">
       <a :href="'https://linux.do/t/topic/' + item.id" target="_blank">
         {{ item.title }}
@@ -12,32 +11,6 @@
 
 <script>
 export default {
-  data() {
-    return {
-      list: [],
-    };
-  },
-  methods: {
-    // 获取热门列表
-    async getHotPosts() {
-      this.list = [];
-      fetch("https://linux.do/new.json")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          this.list = data.topic_list.topics.slice(0, 20);
-          localStorage.setItem('newslist', JSON.stringify(this.list));
-        })
-        .catch((error) => {});
-    },
-  },
-  created() {
-    const newslist = localStorage.getItem('newslist');
-    if (newslist) {
-      this.list = JSON.parse(newslist);
-    } else {
-      this.getHotPosts(); // 获取热门列表
-    }
-  },
+  props: ['list'],
 };
 </script>

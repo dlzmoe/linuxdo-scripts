5c50ead6be51695fc01428fad18c5ffa447e45ef
dlzmoe <65840178+dlzmoe@users.noreply.github.com>
2025-01-17 11:34:18 +0800

    feat: 右上角弹窗记录上一次滚动的位置

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -4,8 +4,10 @@
     <li :class="{ act: activeTab === 'news' }" @click="changeTab('news')">最新</li>
     <li :class="{ act: activeTab === 'menu' }" @click="changeTab('menu')">菜单</li>
   </ul>
-  <div class="refresh" @click="refresh"><svg xmlns="http://www.w3.org/2000/svg" width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg></div>
-  <div class="container">
+  <div class="refresh" @click="refresh">
+    <Refresh />
+  </div>
+  <div class="container" ref="container">
     <div class="content" :class="{ act: activeTab === 'hot' }">
       <HotPosts :list="hotlist" />
     </div>
@@ -22,6 +24,7 @@
 import HotPosts from "./components/HotPosts.vue";
 import NewsPosts from "./components/NewsPosts.vue";
 import SettingMenu from "./components/SettingMenu.vue";
+import Refresh from "./components/SVG/Refresh.vue";
 
 export default {
   data() {
@@ -29,16 +32,32 @@ export default {
       activeTab: "news",
       hotlist: [], // 热门列表
       newslist: [], // 最新列表
+
+      lastScrollPosition: 0, // 存储滚动位置
     };
   },
   components: {
     HotPosts,
     NewsPosts,
     SettingMenu,
+    Refresh,
   },
   methods: {
+    // 平滑滚动到顶部
+    scrollToTop() {
+      const container = this.$refs.container;
+      if (container) {
+        container.scrollTo({
+          top: 0,
+          behavior: 'smooth' // 平滑滚动
+        });
+      }
+    },
+    // 切换菜单
     changeTab(tab) {
       this.activeTab = tab;
+      this.scrollToTop();
+      localStorage.setItem('activeTab', this.activeTab);
     },
     // 获取热门列表
     async getHotPosts() {
@@ -46,7 +65,6 @@ export default {
       fetch("https://linux.do/top.json")
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
           this.hotlist = data.topic_list.topics;
           localStorage.setItem('hotlist', JSON.stringify(this.hotlist));
         })
@@ -58,11 +76,10 @@ export default {
       fetch("https://linux.do/new.json")
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
           this.newslist = data.topic_list.topics;
           localStorage.setItem('newslist', JSON.stringify(this.newslist));
         })
-        .catch((error) => {});
+        .catch((error) => { });
     },
 
     // 刷新
@@ -75,9 +92,47 @@ export default {
         type: "success",
         duration: 1000,
       });
-    }
+    },
+
+    // 当容器滚动时，记录当前位置
+    recordScroll() {
+      const container = this.$refs.container;
+      if (container) {
+        this.lastScrollPosition = container.scrollTop; // 保存滚动位置
+        localStorage.setItem('lastScrollPosition', this.lastScrollPosition);
+      }
+    },
+
+    // 恢复到之前保存的位置
+    restoreScrollPosition() {
+      const container = this.$refs.container;
+      if (container) {
+        container.scrollTo({
+          top: this.lastScrollPosition, // 恢复上次保存的位置
+          // behavior: "smooth", // 平滑滚动
+        });
+      }
+    },
   },
   created() {
+
+    /* 找到上次打开的位置 */
+    const activeTab = localStorage.getItem('activeTab');
+    if (activeTab) {
+      this.activeTab = activeTab;
+    }
+    const lastScrollPosition = localStorage.getItem('lastScrollPosition');
+    if (lastScrollPosition) {
+      this.lastScrollPosition = Number(lastScrollPosition); // 转换为数字
+
+      // 滚动到之前保存的位置
+      setTimeout(() => {
+        this.restoreScrollPosition();
+      }, 1);
+    }
+    this.recordScroll();
+    /* 找到上次打开的位置 */
+
     const Timestamp = localStorage.getItem('Timestamp', Date.now());
     if (Timestamp) {
       const timeDiff = new Date() - Timestamp;
@@ -104,6 +159,20 @@ export default {
       this.getNewsPosts(); // 获取最新列表
     }
 
-  }
+  },
+  // 绑定滚动事件，自动记录滚动位置
+  mounted() {
+    const container = this.$refs.container;
+    if (container) {
+      container.addEventListener("scroll", this.recordScroll); // 监听滚动事件
+    }
+  },
+  // 解绑事件，防止内存泄漏
+  beforeDestroy() {
+    const container = this.$refs.container;
+    if (container) {
+      container.removeEventListener("scroll", this.recordScroll);
+    }
+  },
 };
 </script>
